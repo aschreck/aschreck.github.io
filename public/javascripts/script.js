00abb0bb9adfd2e98dc275f173ee3e7ba819f5c1
Aaron Schreck <schreck.aaron@gmail.com>
2018-03-13 21:00:30 -0600

    add shaq-fu button

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,9 @@
-function getAristotle() {
-    fetch 
+$(document).ready(waitForShaqFu)
+
+function waitForShaqFu() {
+    $(".shaq-btn").on("click", shaqFu)
+}
+
+function shaqFu() {
+    $(".shaq-zone").append(`<img height=200 width=280 src="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr06/2013/9/13/11/enhanced-buzz-3994-1379086824-25.jpg?downsize=715:*&output-format=auto&output-quality=auto">`)
 }
